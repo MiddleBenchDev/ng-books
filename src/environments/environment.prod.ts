063e7ecbe61a7b31fc1b4b15b08eea9505b3b7ae
Starwin <106689136+MiddleBenchDev@users.noreply.github.com>
2022-09-01 21:37:58 +0530

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,22 +1,22 @@
 export const environment = {
   firebase: {
-    projectId: 'books-mbd',
-    appId: '1:935892854974:web:730f5a1b17b466dda616ef',
-    storageBucket: 'books-mbd.appspot.com',
-    locationId: 'us-central',
-    apiKey: 'AIzaSyARAcLP_VQWzLkAbhQJwLvJBothPIOKD-8',
-    authDomain: 'books-mbd.firebaseapp.com',
-    messagingSenderId: '935892854974',
-    measurementId: 'G-E1LMWEPEXC',
+    projectId: '',
+    appId: '',
+    storageBucket: '',
+    locationId: '',
+    apiKey: '',
+    authDomain: '',
+    messagingSenderId: '',
+    measurementId: '',
   },
   production: true,
   firebaseConfig: {
-    apiKey: "AIzaSyARAcLP_VQWzLkAbhQJwLvJBothPIOKD-8",
-    authDomain: "books-mbd.firebaseapp.com",
-    projectId: "books-mbd",
-    storageBucket: "books-mbd.appspot.com",
-    messagingSenderId: "935892854974",
-    appId: "1:935892854974:web:730f5a1b17b466dda616ef",
-    measurementId: "G-E1LMWEPEXC"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
   },
 };
